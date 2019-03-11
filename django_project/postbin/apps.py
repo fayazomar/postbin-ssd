54aef7e1faf7d01c8bd317a87e318af0113d8dd5
Fayaz <fayaz@Fayazs-MBP.cgocable.net>
2019-03-11 17:22:52 -0400

    Second commit

---
+++ django_project/postbin/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PostbinConfig(AppConfig):
+    name = 'postbin'
--- django_project/postbin/urls.py
+++ django_project/postbin/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from postbin import views
+from . import views
 
 urlpatterns = [
     path('', views.home, name='Postbin-home'),
