7b065ebe07730e015f70f31881cbab18aa629018
Fayaz <fayaz@Fayazs-MBP.cgocable.net>
2019-03-11 21:11:15 -0400

    Fourth commit

--- django_project/postbin/views.py
+++ django_project/postbin/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Fayaz Omar',
-        'title': 'Post 1',
-        'content': 'First post content',
-        'date_posted': 'November 1, 2018'
-    },
-    {
-        'author': 'Abbas Ali',
-        'title': 'Post 2',
-        'content': 'Second post content',
-        'date_posted': 'November 2, 2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'postbin/home.html', context)
 
