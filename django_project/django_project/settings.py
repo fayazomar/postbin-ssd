9fcc54fec5bf1c29eff6274fba6e5efa34c50437
Fayaz <fayaz@Fayazs-MBP.cgocable.net>
2019-03-11 21:33:07 -0400

    Fifth commit

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -22,6 +22,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'postbin.apps.PostbinConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -109,3 +111,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
